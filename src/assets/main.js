39f283275985e3d8a4ab426f8e8b47ff15f28652
jasondeving <aznj50@hotmail.com>
2016-12-08 21:07:06 -0800

    more code

--- src/assets/main.js
+++ src/assets/main.js
@@ -18,6 +18,13 @@ $(function() {
   		}).appendTo($badges);
 
   		$('<h3 />', {text: course.title}).appendTo($course);
+  		$('<img />', {src: course.badge}).appendTo($course);
+  		$('<a />', {
+  			'class': 'btn btn-primary',
+  			target: '_blank',
+  			href: 'course.url',
+  			text: 'See Course'
+  		}).appendTo($course);
   	})
   }
 });
